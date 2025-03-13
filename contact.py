847bc7d6ea5fe1d33d0071bb1c5f88d3c25b9869
Bhargav Hira <bhargavprotim000hira@gmail.com>
2025-03-13 15:58:08 +0530

    [Bhargav] Add : Script to store contacts in the Address Book

---
+++ contact.py
@@ -0,0 +1,11 @@
+class Contact:
+    
+    def __init__(self, first_name, last_name, address, city, state, zip, phone_number, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.phone_number = phone_number
+        self.email = email
